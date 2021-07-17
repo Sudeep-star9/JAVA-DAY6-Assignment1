c05456fdcd72baf483848cc7455e78ea17e68f7e
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-07-17 06:14:05 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function myfun(num){
+    for(var i=0;i<num;i++){
+        console.log(i)
+    }
+
+}
+myfun(5)
+
+
+//for loop using in array
+var cars=["Bmw","Mustang","Audi","Mercedes","Astonmartin","Porche","Lambo"]
+for(var i=0;i<cars.length;i++){
+    console.log(cars[i])
+}
